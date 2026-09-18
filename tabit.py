6772f2636499496a67542dde308a1cf152f65616
ChrisLi <chris.li@senao.com>
2026-09-18 09:19:54 +0800

    v1.7.7: Find, tmux list, work-group sidebar, T2/T3/T5 fixes

--- tabit.py
+++ tabit.py
@@ -292,7 +292,7 @@ DEFAULT_AI_CLIS = [
     {"cli": "agy", "try": ["-c", "--continue"]},
 ]
 # used when user types a CLI not in the list
-APP_VERSION = "v1.7.6"
+APP_VERSION = "v1.7.7"
 
 
 def _get_tabit_repo_dir():
